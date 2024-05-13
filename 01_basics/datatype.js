80237552a00688a52800cb7f505816725385b5aa
Poojandelvadiya <167162051+Poojandelvadiya@users.noreply.github.com>
2024-05-13 08:46:52 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+// Primitive datatype:String,number,boolearn,null,undefined,symbol,Bigint
+// Non-primitive(reference): Array,objects,Function
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 324567654765469876n
+
+const heros = ["poojan","akshay","ayush"];
+let myObj = {
+    name: "poojan",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
