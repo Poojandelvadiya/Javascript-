b1fb65e9e3d9171bc59ce9551751d8ebea01cbe4
Poojandelvadiya <167162051+Poojandelvadiya@users.noreply.github.com>
2024-05-13 08:25:29 +0000

    comparison

--- 01_basics/03_cn.js
+++ 01_basics/03_cn.js
@@ -14,5 +14,9 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log (stringNumber);
-console.log (typeof stringNumber);
+// console.log (stringNumber);
+// console.log (typeof stringNumber);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
